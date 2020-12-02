318a3190fbc83cd4515d3df7eefb423403ef11fb
TEX-2 pc <pshcyrill@mail.ru>
2020-12-02 16:29:07 +0300

    add endline

--- LogClass.cpp
+++ LogClass.cpp
@@ -16,7 +16,7 @@ void LogClass::write(std::string text){
 
 void LogClass::write4values(double x1,double x2,double y1, double y2,bool correct){
 	lf << "\t" << "x1 = " << x1 << ", x2 = " << x2 << ", y1 = " << y1 << ", y2 = " << y2;
-	if(correct) lf << ", 1";
-	else lf << ", 0";
+	if(correct) lf << ", 1" << std::endl;
+	else lf << ", 0" << std::endl;
 	return;
 }
